7634139ada282318e8eca0d00d77a9ec851319a2
shivanee <shivanimande@gmailcom>
2023-04-21 17:33:29 +0530

    customer table

---
+++ cust_table.sql
@@ -0,0 +1,11 @@
+create table Mcustomer(
+    custid int primary key,
+    cname varchar(50) not null,
+    cmobile varchar(20)
+);
+
+insert into Mcustomer values(1,"Amit","7548123695");
+insert into Mcustomer values(2,"Anjali","9548123656");
+insert into Mcustomer values(3,"Tanaya","7848123695");
+insert into Mcustomer values(4,"Bidipta","8048173675");
+insert into Mcustomer values(5,"Samara","9478216347");
